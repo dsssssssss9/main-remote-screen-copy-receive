a2d2ac52411ef7de278a7bfd33c41efd8cc073c8
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-29 19:22:28 +0100

    init colorbit & radio

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 radio.sendNumber(42)
 basic.showString("CQ CQ")
+let colorbit_51bit = colorbit.initColorBit(DigitalPin.P15, BitColorMode.RGB)
+colorbit_51bit.clear()
+colorbit_51bit.setBrightness(16)
+colorbit_51bit.show()
 basic.forever(function () {
 	
 })
